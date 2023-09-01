f2d727970e6979749409884adf6c73d10ebf30d5
Joshen Lim <77193243+joshenx@users.noreply.github.com>
2023-09-02 02:48:14 +0800

    Add calistoga

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
+import '@fontsource/calistoga/400.css';
 
 import { Box, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
